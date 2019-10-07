a66985c43d8be656349fc68fc132d6b8aacc909a
Shreyans Sonthalia <ssshreyans26@gmail.com>
2019-10-07 15:50:09 +0530

    student delete route added

--- controllers/student.js
+++ controllers/student.js
@@ -40,3 +40,18 @@ exports.post_student_form = (req,res,next) => {
       }
 }
 
+exports.delete_student_form = (req, res, next) => {
+    //console.log(req.body.data)
+    var id = req.body.data;
+    Student_Form.findByIdAndRemove(req.params.todoId, (err, todo) => {
+        // As always, handle any potential errors:
+        if (err) return res.status(500).send(err);
+        // We'll create a simple object to send back with a message and the id of the document that was removed
+        // You can really do this however you want, though.
+        const response = {
+            message: "Student Data  deleted successfully ",
+            id: todo._id
+        };
+        return res.status(200).send(response);
+    });
+  }
--- routes/student.js
+++ routes/student.js
@@ -5,7 +5,8 @@ var router = express.Router();
 
 router.get('/get_student_form', studentController.get_student_form);
 
-
 router.post('/post_student_form', studentController.post_student_form);
+
+router.post('/delete_student_form', studentController.delete_student_form);
 
 module.exports = router;
